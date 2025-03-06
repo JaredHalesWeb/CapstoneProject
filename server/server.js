d6cb50d3d6072fb18fbbd21fba6d3d4d03afcb57
jared hales <jared.hales2838@STU.MTEC.EDU>
2025-03-06 08:14:50 -0700

    base layed down

--- server/server.js
+++ server/server.js
@@ -6,13 +6,15 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, '../client/dist')))
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+});
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
